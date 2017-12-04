07aab20bd1852525ba7cec38efee7f94fa898648
yaochenglong <289911401@qq.com>
2017-12-04 22:19:02 +0800

    1.第一章，服务注册，发现，消费

---
+++ service-ribbon/src/main/java/com/yao/controller/HelloControler.java
@@ -0,0 +1,20 @@
+package com.yao.controller;
+
+import com.yao.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yaochenglong on 17/12/4.
+ */
+@RestController
+public class HelloControler {
+    @Autowired
+    HelloService helloService;
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+}
