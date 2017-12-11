77db1fae9abbdef8155c5af9a202879f3d76dce3
yaochenglong <289911401@qq.com>
2017-12-11 19:35:42 +0800

    服务链路追踪

--- service-hi/src/main/java/com/yao/ServiceHiApplication.java
+++ service-hi/src/main/java/com/yao/ServiceHiApplication.java
@@ -1,26 +1,62 @@
 package com.yao;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @RestController
 public class ServiceHiApplication {
+
+    private Logger logger = Logger.getLogger(this.getClass());
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceHiApplication.class, args);
 	}
 
-	@Value("${server.port}")
+	/*@Value("${server.port}")
 	String port;
 	@RequestMapping("/hi")
 	public String home(@RequestParam String name) {
 		return "hi "+name+",i am from port:" +port;
+	}*/
+
+
+	////////////////////对外暴露接口//////////////////////
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
 	}
+
+	@RequestMapping("/hi")
+	public String callHome(){
+		logger.log(Level.INFO, "calling trace service-hi  ");
+		return restTemplate.getForObject("http://localhost:8989/miya", String.class);
+	}
+
+	@RequestMapping("/info")
+	public String info(){
+		logger.log(Level.INFO, "calling trace service-hi ");
+
+		return "i'm service-hi";
+
+	}
+
+	@Bean
+	public AlwaysSampler defaultSampler(){
+		return new AlwaysSampler();
+	}
+
 }
